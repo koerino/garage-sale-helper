3a8c56388d417fd15accfba432aa9f7cf286062d
koerino <koerino@gmail.com>
2016-06-06 18:53:41 -0400

    add auth control flow and more structure

--- app/controllers/AuthController.java
+++ app/controllers/AuthController.java
@@ -4,10 +4,13 @@ import play.mvc.*;
 
 import models.User;
 
-import views.html.*;
+import views.html.auth.*;
 
 public class AuthController extends Controller {
     public Result index() {
         return ok(login.render());
+    }
+    public Result signup() {
+        return ok(signup.render());
     }
 }
