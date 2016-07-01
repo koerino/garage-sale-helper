b02a5a37080238f761c321b22ea1a49eca5fc81e
koerino <koerino@gmail.com>
2016-06-30 21:53:43 -0400

    add features: update and search items

--- app/controllers/SaleController.java
+++ app/controllers/SaleController.java
@@ -5,12 +5,22 @@ import play.mvc.Result;
 import play.data.Form;
 
 import models.Sale;
+import models.Item;
 
-import views.html.user.sales;
+import java.util.List;
+
+import views.html.sale.*;
 
 public class SaleController extends Controller {
     
-    private int idGenerator = 0;
+    /**
+     * Get the catalog of a specific sale given the sale id
+     */
+    public Result index(int id) {
+        Sale sale = Sale.find.byId(id);
+        List<Item> list = Item.find.where().eq("saleId", id).findList();
+        return ok(catalog.render(sale, list));
+    }
     
     /**
      * Save a new sale to the database
@@ -23,7 +33,6 @@ public class SaleController extends Controller {
         
         newSale.setTitle(title);
         newSale.setLocation(location);
-        newSale.setSaleId(idGenerator++);
         newSale.setSellerId(session().get("username"));
         newSale.setSize(0);
         
@@ -31,4 +40,55 @@ public class SaleController extends Controller {
         return ok("Sale added successfully.");
     }
     
+    /**
+     * Get a specific item given id
+     */
+    public Result getItem(int id) {
+        Item entry = Item.find.byId(id);
+        Sale sale = Sale.find.byId(entry.getSaleId());
+        return ok(item.render(sale, entry));
+    }
+    
+    /**
+     * Add an item to the catalog of the chosen sale
+     */
+    public Result addItem() {
+        Item newItem = new Item();
+        
+        String title = Form.form().bindFromRequest().get("title");
+        String description = Form.form().bindFromRequest().get("description");
+        double price = Double.parseDouble(Form.form().bindFromRequest().get("price"));
+        int stock = Integer.parseInt(Form.form().bindFromRequest().get("stock"));
+        int saleId = Integer.parseInt(Form.form().bindFromRequest().get("saleId"));
+                
+        newItem.setTitle(title);
+        newItem.setDescription(description);
+        newItem.setPrice(price);
+        newItem.setStock(stock);
+        newItem.setSaleId(saleId);
+                
+        newItem.save();
+        return ok("Item added successfully.");
+    }
+    
+    /**
+     * Update an item
+     */
+    public Result editItem() {
+        String title = Form.form().bindFromRequest().get("title");
+        String description = Form.form().bindFromRequest().get("description");
+        String priceStr = Form.form().bindFromRequest().get("price");
+        String stockStr = Form.form().bindFromRequest().get("stock");
+        int itemId = Integer.parseInt(Form.form().bindFromRequest().get("itemId"));
+        
+        Item item = Item.find.byId(itemId);
+        
+        if (!title.isEmpty()) item.setTitle(title);
+        if (!description.isEmpty()) item.setDescription(description);
+        if (!priceStr.isEmpty()) item.setPrice(Double.parseDouble(priceStr));
+        if (!stockStr.isEmpty()) item.setStock(Integer.parseInt(stockStr));
+        item.save();
+        
+        return ok("Item updated successfully.");
+    }
 }
